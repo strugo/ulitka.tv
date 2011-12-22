22f299d8f5fc93a89cd19c327ae07896030babf8
Geotgiy O. Struve <iamstrugo@gmail.com>
2011-12-22 17:14:39 +0700

    init

---
+++ default.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import sys, os
+
+sys.path.append(os.path.join(os.getcwd().replace(';', ''), 'resources', 'lib'))
+
+if (__name__ == "__main__" ):
+	import addon
+	addon.addon_main()
